cbb0209157cff7168282eb6b43b0cadebd3ef308
IsaakThaddaeus <66296094+IsaakThaddaeus@users.noreply.github.com>
2022-11-07 11:09:25 +0100

    Add files via upload

---
+++ Web/js.js
@@ -0,0 +1,62 @@
+
+const sendeDaten = async () => {
+    const response = await fetch("https://chat.web2021.dhbw.scytec.de/room/888/messages", {
+      method: "POST",
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({sender: "tomaso", text: " Herr Koch ist cool", data: ""})
+    });
+
+    const json = await response.json();
+    console.log(json);
+  }
+
+  const ladeDaten = async () => {
+    const response = await fetch("https://chat.web2021.dhbw.scytec.de/room/888/messages");
+    const json = await response.json();
+    console.log(json);
+  }
+
+
+  var name;
+  var gruppe;
+  var gruppenN;
+ 
+  function login (form) {
+     this.name = form.name.value;
+     console.log(this.name);
+     window.location.href = 'main.html'
+  }
+
+  function gruppeName (form) {
+    this.gruppe = form.gruppe.value;
+    console.log(this.gruppe);
+    document.getElementById("gruppenNameId").style.visibility = "hidden";
+ }
+
+  function gruppenNachricht (form)
+  {
+    this.gruppenN = form.gruppenN.value;
+    console.log(this.gruppenN);
+
+  }
+
+  function groupClick(){
+    console.log(this.name);
+    document.getElementById("gruppenNameId").style.visibility = "visible";
+  }
+
+  function dmClick(){
+    console.log(this.name);
+  }
+
+  function logoutClick(){
+    window.location.href = 'index.html'
+  }
+
+
+
+  //sendeDaten();
+
+  ladeDaten();
